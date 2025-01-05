d466381f0df4a2b4ae0e2abb789cf682a30dc7e6
John Calvi <jcalvi@lewis.com.au>
2025-01-05 18:28:59 +1100

    Try remove warning 3rd attempt

--- BuildAll.bat
+++ BuildAll.bat
@@ -8,9 +8,9 @@ docker build -t jcrfc/hass-actronque-amd64:latest -t jcrfc/hass-actronque-amd64:
 docker push jcrfc/hass-actronque-amd64:latest
 docker push jcrfc/hass-actronque-amd64:2025.1.0
 
-docker build -t jcrfc/hass-actronque-armv7:latest -t jcrfc/hass-actronque-armv7:2025.1.0 . --platform linux/arm/v7
-docker push jcrfc/hass-actronque-armv7:latest
-docker push jcrfc/hass-actronque-armv7:2025.1.0
+rem docker build -t jcrfc/hass-actronque-armv7:latest -t jcrfc/hass-actronque-armv7:2025.1.0 . --platform linux/arm/v7
+rem docker push jcrfc/hass-actronque-armv7:latest
+rem docker push jcrfc/hass-actronque-armv7:2025.1.0
 
 pause
 
