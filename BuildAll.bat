a4a874900dfff1b2b3d11bad41e1dcfa5fda51e4
John Calvi <jcalvi@lewis.com.au>
2025-01-05 16:15:05 +1100

    Fix Port Warning

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,3 +1,5 @@
+cd .\hass-actronque
+
 docker build -t jcrfc/hass-actronque-aarch64:latest -t jcrfc/hass-actronque-aarch64:2025.1.0 . --platform linux/arm64
 docker push jcrfc/hass-actronque-aarch64:latest
 docker push jcrfc/hass-actronque-aarch64:2025.1.0
@@ -7,12 +9,12 @@ docker push jcrfc/hass-actronque-amd64:latest
 docker push jcrfc/hass-actronque-amd64:2025.1.0
 
 docker build -t jcrfc/hass-actronque-armv7:latest -t jcrfc/hass-actronque-armv7:2025.1.0 . --platform linux/arm/v7
-
 docker push jcrfc/hass-actronque-armv7:latest
 docker push jcrfc/hass-actronque-armv7:2025.1.0
 
+pause
 
 
-docker tag jcrfc/hass-actronque-aarch64 jcrfc/hass-actronque-aarch64
-docker tag jcrfc/hass-actronque-amd64 jcrfc/hass-actronque-amd64
-docker tag jcrfc/hass-actronque-armv7 jcrfc/hass-actronque-armv7
+rem docker tag jcrfc/hass-actronque-aarch64 jcrfc/hass-actronque-aarch64
+rem docker tag jcrfc/hass-actronque-amd64 jcrfc/hass-actronque-amd64
+rem docker tag jcrfc/hass-actronque-armv7 jcrfc/hass-actronque-armv7
