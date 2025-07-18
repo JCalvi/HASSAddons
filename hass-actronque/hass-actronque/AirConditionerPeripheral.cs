5cc6685de15a191df3eb17b4bb3c3942e8971043
John Calvi <jcalvi@lewis.com.au>
2025-07-18 16:51:43 +1000

    Attempt 2 to sync with MJM 2025.7.2

---
+++ hass-actronque/hass-actronque/AirConditionerPeripheral.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMX.HASSActronQue
+{
+	public class AirConditionerPeripheral
+	{
+		public string SerialNumber; // SerialNumber
+		public string DeviceType; // DeviceType
+		public double Battery; // RemainingBatteryCapacity_pc
+
+		public AirConditionerPeripheral()
+		{
+			Battery = 0;
+		}
+	}
+}
