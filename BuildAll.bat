632f6f3d9b05d5cfcc4016f1705434be3a94faf1
John Calvi <jcalvi@lewis.com.au>
2026-01-09 16:49:31 +1100

    Fix bugs, add default entity id

--- BuildAll.bat
+++ BuildAll.bat
@@ -7,15 +7,13 @@ Powershell -Command "& {(Get-Content .\config.json) -replace '\d{4}.\d+.\d+', '%
 
 echo %date% > .\hass-actronque\Resources\BuildDate.txt
 
+docker build -t jcrfc/hass-actronque-amd64:latest -t jcrfc/hass-actronque-amd64:%jcversion% . --platform linux/amd64
+docker push jcrfc/hass-actronque-amd64:latest
+docker push jcrfc/hass-actronque-amd64:%jcversion%
 
 docker build -t jcrfc/hass-actronque-aarch64:latest -t jcrfc/hass-actronque-aarch64:%jcversion% . --platform linux/arm64
 docker push jcrfc/hass-actronque-aarch64:latest
 docker push jcrfc/hass-actronque-aarch64:%jcversion%
-
-
-docker build -t jcrfc/hass-actronque-amd64:latest -t jcrfc/hass-actronque-amd64:%jcversion% . --platform linux/amd64
-docker push jcrfc/hass-actronque-amd64:latest
-docker push jcrfc/hass-actronque-amd64:%jcversion%
 
 rem Armv7 Deprecated.
 rem docker build -t jcrfc/hass-actronque-armv7:latest -t jcrfc/hass-actronque-armv7:%jcversion% . --platform linux/arm/v7
