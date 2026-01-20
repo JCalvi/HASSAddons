b19c56a578b5a884e15a30800d98bcdfcd843e37
John Calvi <jcalvi@lewis.com.au>
2026-01-21 09:01:16 +1100

    Vers bump

--- hass-actronque/hass-actronque/Que.cs
+++ hass-actronque/hass-actronque/Que.cs
@@ -24,7 +24,7 @@ namespace HMX.HASSActronQue
 			RecreateHttpClients();
 
 			// updated version marker for this build
-			Logging.WriteDebugLog("Que.Que(v2026.1.6.12)");
+			Logging.WriteDebugLog("Que.Que(v2026.1.6.13)");
 		}
 
 		// Changed to Task so callers can observe failures
