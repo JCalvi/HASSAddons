bceddccfea69fdc83dc60d9034653695eaa12f65
John Calvi <jcalvi@lewis.com.au>
2026-03-02 17:23:06 +1100

    Update names to match hass items.

--- Build-Rekognition.bat
+++ Build-Rekognition.bat
@@ -1,5 +1,5 @@
 rem https://github.com/dotnet/dotnet-docker/blob/main/README.aspnet.md#full-tag-listing
-cd .\rekognition_bridge
+cd .\hass-rekognition
 
 set jcversion=2026.3.1
 
@@ -7,12 +7,12 @@ Powershell -Command "& {(Get-Content .\config.json) -replace '\d{4}.\d+.\d+', '%
 
 rem echo %date% > .\hass-actronque\Resources\BuildDate.txt
 
-docker build -t jcrfc/ha-rekognition-amd64:latest -t jcrfc/ha-rekognition-amd64:%jcversion% . --platform linux/amd64
-docker push jcrfc/ha-rekognition-amd64:latest
-docker push jcrfc/ha-rekognition-amd64:%jcversion%
+docker build -t jcrfc/hass-rekognition-amd64:latest -t jcrfc/hass-rekognition-amd64:%jcversion% . --platform linux/amd64
+docker push jcrfc/hass-rekognition-amd64:latest
+docker push jcrfc/hass-rekognition-amd64:%jcversion%
 
-docker build -t jcrfc/ha-rekognition-aarch64:latest -t jcrfc/ha-rekognition-aarch64:%jcversion% . --platform linux/arm64
-docker push jcrfc/ha-rekognition-aarch64:latest
-docker push jcrfc/ha-rekognition-aarch64:%jcversion%
+docker build -t jcrfc/hass-rekognition-aarch64:latest -t jcrfc/hass-rekognition-aarch64:%jcversion% . --platform linux/arm64
+docker push jcrfc/hass-rekognition-aarch64:latest
+docker push jcrfc/hass-rekognition-aarch64:%jcversion%
 
 pause
